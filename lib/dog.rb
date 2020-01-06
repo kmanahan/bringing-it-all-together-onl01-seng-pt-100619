3ca8a665703146f25b7bb26c71aa185312491439
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-06 20:14:39 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,10 +36,11 @@ class Dog
         DB[:conn].execute(sql, self.name, self.breed)
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
       end 
+      self
     end 
     
      def self.create(name:, breed:)
-      new_dog = Dog.new(name, breed)
+      new_dog = Dog.new(name: name, breed: breed)
       new_dog.save 
       new_dog
     end 
@@ -60,8 +61,31 @@ class Dog
     
     def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
-    result = DB[:conn].execute(sql, id)[0]
-    Song.new(result[0], result[1], result[2])
+    DB[:conn].execute(sql,id).map do |row|
+      self.new_from_db(row)
+      end.first
+    end
+    
+  #   def self.find_or_create_by(name:, album:)
+  #   song = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND album = ?", name, album)
+  #   if !song.empty?
+  #     song_data = song[0]
+  #     song = Song.new(song_data[0], song_data[1], song_data[2])
+  #   else
+  #     song = self.create(name: name, album: album)
+  #   end
+  #   song
+  # end
+    
+    def self.find_or_create_by(name:, breed:)
+      dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+      if !dog.empty? 
+        new_dog = dog[0]
+        dog = self.new(new_dog[0], new_dog[1], new_dog[2])
+      else 
+        dog = self.create(name: name, breed: breed)
+      end 
+      dog
     end
     
     def update 
