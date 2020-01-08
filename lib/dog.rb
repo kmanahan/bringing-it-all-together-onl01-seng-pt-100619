c7fa6263bd98f292659bcec942eeb93704592958
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-08 17:20:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,16 +66,6 @@ class Dog
       end.first
     end
     
-  #   def self.find_or_create_by(name:, album:)
-  #   song = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND album = ?", name, album)
-  #   if !song.empty?
-  #     song_data = song[0]
-  #     song = Song.new(song_data[0], song_data[1], song_data[2])
-  #   else
-  #     song = self.create(name: name, album: album)
-  #   end
-  #   song
-  # end
     
     def self.find_or_create_by(name:, breed:)
       dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
